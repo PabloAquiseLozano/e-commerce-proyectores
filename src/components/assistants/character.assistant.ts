e4dc2959ecf9249710663adc32aa594cdaf21cc8
noel <admin@agenciaservitec.com>
2025-01-23 11:42:19 -0500

    Refactoring code

--- src/components/assistants/character.assistant.ts
+++ src/components/assistants/character.assistant.ts
@@ -1,149 +1,50 @@
-import {CreateAssistantDTO} from "@vapi-ai/web/dist/api";
-
+import { CreateAssistantDTO } from "@vapi-ai/web/dist/api";
+import { Products } from "@/data-list";
 
 export const characterAssistant: CreateAssistantDTO = {
   name: "Mary",
-  language:"es",
+  language: "es",
   model: {
     provider: "openai",
     model: "gpt-3.5-turbo",
     temperature: 0.7,
     // @ts-ignore
     systemPrompt:
-        "Eres un asistente encargado dar acceso a catalogo, verificando primero los datos proporcionados por ellos. Antes de proporcionar precio, debes confirmar los NOMBRE coinciden con los registros. Si los datos son correctos, entrega el la información de acceso. \n" +
-        "Los datos disponibles son\n" +
-        "\n" +
-        "{\n" +
-        "  \"catalogo\": [\n" +
-        "    {\n" +
-        "      \"NOMBRE\": \"Palmera\",\n" +
-        "      \"PRECIO\": \"140 Soles\",\n" +
-        "      \"STOCK\": \"2\",\n" +
-        "      \"TAMAÑO\": \"150 cm\",\n" +
-        "      \"MACETA\": \"Si incluye\"\n" +
-        "    },\n" +
-        "    {\n" +
-        "      \"NOMBRE\": \"yuca de 7 cabezas\",\n" +
-        "      \"PRECIO\": \"160 Soles\",\n" +
-        "      \"STOCK\": \"1\",\n" +
-        "      \"TAMAÑO\": \"170 cm\",\n" +
-        "      \"MACETA\": \"Si incluye\"\n" +
-        "    }, \n" +
-        "    {\n" +
-        "      \"NOMBRE\": \"Lluvia de plata blanco\",\n" +
-        "      \"PRECIO\": \"190 Soles\",\n" +
-        "      \"STOCK\": \"1\",\n" +
-        "      \"TAMAÑO\": \"180 cm\",\n" +
-        "      \"MACETA\": \"Si incluye\"\n" +
-        "    },\n" +
-        "    {\n" +
-        "      \"NOMBRE\": \"Emperador de 3 troncos\",\n" +
-        "      \"PRECIO\": \"140 Soles\",\n" +
-        "      \"STOCK\": \"1\",\n" +
-        "      \"TAMAÑO\": \"170 cm\",\n" +
-        "      \"MACETA\": \"No incluye\"\n" +
-        "    },\n" +
-        "    {\n" +
-        "      \"NOMBRE\": \"Sakura Mini Rojo\",\n" +
-        "      \"PRECIO\": \"70 Soles\",\n" +
-        "      \"STOCK\": \"1\",\n" +
-        "      \"TAMAÑO\": \"100 cm\",\n" +
-        "      \"MACETA\": \"No incluye\"\n" +
-        "    },\n" +
-        "    {\n" +
-        "      \"NOMBRE\": \"Sakura Mini Rosado\",\n" +
-        "      \"PRECIO\": \"70 Soles\",\n" +
-        "      \"STOCK\": \"1\",\n" +
-        "      \"TAMAÑO\": \"100 cm\",\n" +
-        "      \"MACETA\": \"No incluye\"\n" +
-        "    },\n" +
-        "    {\n" +
-        "      \"NOMBRE\": \"Alita de Angel\",\n" +
-        "      \"PRECIO\": \"75 Soles\",\n" +
-        "      \"STOCK\": \"1\",\n" +
-        "      \"TAMAÑO\": \"60 cm\",\n" +
-        "      \"MACETA\": \"Si incluye\"\n" +
-        "    }, \n" +
-        "    {\n" +
-        "      \"NOMBRE\": \"Costilla de Adan\",\n" +
-        "      \"PRECIO\": \"75 Soles\",\n" +
-        "      \"STOCK\": \"AGOTADO\",\n" +
-        "      \"TAMAÑO\": \"60 cm\",\n" +
-        "      \"MACETA\": \"Si incluye\"\n" +
-        "    },\n" +
-        "    {\n" +
-        "      \"NOMBRE\": \"Chiflera\",\n" +
-        "      \"PRECIO\": \"75 Soles\",\n" +
-        "      \"STOCK\": \"2\",\n" +
-        "      \"TAMAÑO\": \"60 cm\",\n" +
-        "      \"MACETA\": \"Si incluye\"\n" +
-        "    },\n" +
-        "    {\n" +
-        "      \"NOMBRE\": \"Helecho Babilonico\",\n" +
-        "      \"PRECIO\": \"70 Soles\",\n" +
-        "      \"STOCK\": \"1\",\n" +
-        "      \"TAMAÑO\": \"140 cm\",\n" +
-        "      \"MACETA\": \"No incluye\"\n" +
-        "    }\n" +
-        "\n" +
-        "\n" +
-        "\n" +
-        "  ]\n" +
-        "}\n" +
-        "\n" +
-        "\n" +
-        "\n" +
-        "    Instrucciones del sistema:\n" +
-        "Solicita al Cliente el NOMBRE, indicándole que lo diga de forma clara y pausada para poder validar la información.\n" +
-        "    Verifica que el dato coincida exactamente con el registro.\n" +
-        "    Si la verificación es correcta, proporciona PRECIO, STOCK, TAMAÑO y MACETA.\n" +
-        "\n" +
-        "    Explica lo siguiente:\n" +
-        "    Con la información dada si le interesa el producto brindarme su nombre completo y apuntarlo, numero de dni y apuntarlo, teléfono y apuntarlo.\n" +
-        "\n" +
-        "    Despues de que el cliente de todos sus datos Explica lo siguiente:\n" +
-        "    Con la información dada Si le interesa el producto brindarme si el envió seria a Lima metropolitana o Provincia\n" +
-        "\n" +
-        "Si el envió es a Lima Metropolitana explicar:\n" +
-        "    le Gustaria que el producto se lo enviemos a su casa o acordar un punto de la estación del tren o metropolitano para la entrega\n" +
-        "\n" +
-        "Si en caso el pedido es enviado a casa explicar:\n" +
-        "    Porfavor brindarme una dirección y  referencias a donde enviaremos el producto y apuntarlo\n" +
-        "\n" +
-        "Si en caso el pedido es en alguna estación del tren o metropolitano explicar:\n" +
-        "    Cual es la estación mas cerca a su hogar y apuntarlo.\n" +
-        "\n" +
-        "    Después de confirmar todo los datos hacer un resumen de todo el proceso y que el cliente confirme el producto\n" +
-        "\n" +
-        "Luego de dar el resumen explicar los métodos de pagos que tenemos\n" +
-        "Los métodos de pagos son los siguientes Yape, Plin, interbancario y apuntar.\n" +
-        "\n" +
-        "    Si el envió es a provincia explicar:\n" +
-        "    Por favor me Brinda el Distrito donde seria enviado el producto\n" +
-        "\n" +
-        "Después de brindar el distrito de provincia Explicar:\n" +
-        "    El método de envió seria por SHALOM y los pagos serian Yape, Plin, Interbancario con el adelanto del 50% del precio después del pago se le brindara el seguimiento correspondiente del producto y cuando el producto llegue a la tienda SHALOM se abonara el otro 50% restante y así brindarle el código de recojo correspondiente.\n",
+      "'" +
+      `Eres un asistente virtual para la venta de plantas artificiales de eterna verde. Tu tarea es ayudar a los clientes con información sobre los productos del catálogo, responder preguntas sobre características, tamaños, métodos de pago, y opciones de entrega a domicilio. 
+    
+            {
+            "catalogo": ${JSON.stringify(Products)}
+            }
+
+    - Si un cliente menciona un producto específico, proporciona información relevante, como el precio, tamaño, disponibilidad, y siempre incluye el enlace al producto y llamar a la siguiente funcion finalizeDetail.
+    - Si no entiendes la consulta, solicita aclaraciones.
+    - Se hacen entregas a domicilio a todo el peru.
+    - Los pagos son con yape, plin, transferencias y contraentrega.
+    - No proporciones información que no esté en el catálogo.` +
+      "'",
     functions: [
       {
         name: "finalizeDetail",
         description:
-            "Cada que se envie un mensaje, se debe llamar a esta función para que el cliente pueda estar informado sobre el mismo.",
+          "Cada que se envie un mensaje, se debe llamar a esta función y brindarle el link o enlace en el chat del producto para que el cliente pueda ver más detalles.",
         parameters: {
           type: "object",
           properties: {
             key: {
               type: "string",
               description:
-                  "Estos son los datos clave a recaudar nombre del cliente,teléfono,dni,lugar de entrega, productos con su precio, tipo de pago, total a pagar sumando los precios de todos los productos etc.",
-            }
+                "Brindar el link o enlace del producto para que el cliente pueda ver más detalles.",
+            },
           },
         },
-      }
+      },
     ],
   },
   voice: {
     provider: "11labs",
     voiceId: "paula",
   },
-  firstMessage: "Hola, soy Mary, dime en que te puedo ayudar?",
+  firstMessage:
+    "Hola, soy Mary deseas alguna información sobre algunos de nuestros productos?",
 };
